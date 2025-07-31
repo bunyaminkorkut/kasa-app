0a484a35c9cf2cfef5cafa567e70450cc90368b1
bunyaminkorkut <krktbunyamin@gmail.com>
2025-07-31 20:16:42 +0300

    notification base added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -8,7 +9,7 @@ import 'package:kasa_app/application/photo/photo_cubit.dart';
 import 'package:kasa_app/domain/group/i_group_repository.dart';
 import 'package:kasa_app/domain/photo/i_photo_repository.dart';
 import 'package:kasa_app/infrastructure/group/impl_group_service.dart';
-import 'package:kasa_app/infrastructure/photo/impl_photo_service.dart'; // <-- bunu ekle
+import 'package:kasa_app/infrastructure/photo/impl_photo_service.dart';
 import 'package:kasa_app/presentation/home/home.dart';
 import 'package:kasa_app/presentation/login/login.dart';
 import 'package:kasa_app/presentation/register/register.dart';
@@ -17,6 +18,7 @@ import 'package:kasa_app/setup_bindings.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupBindings();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
@@ -25,34 +27,61 @@ void main() async {
   runApp(MyApp(secureStorage: secureStorage));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final FlutterSecureStorage secureStorage;
 
   const MyApp({super.key, required this.secureStorage});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // // iOS için izin iste
+    // _messaging.requestPermission();
+
+    // // Token'ı al
+    // _messaging.getToken().then((token) {
+    //   print("FCM Token: $token");
+    //   // Bu token'ı backend'e kayıt edip push bildirim gönderiminde kullanabilirsin
+    // });
+
+    // // Uygulama açıkken gelen mesajlar için listener
+    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    //   print('Foreground mesajı alındı: ${message.notification?.title}');
+    //   // İstersen local notification göster
+    // });
+
+    // // Uygulama arka plandayken veya kapalıyken tıklanarak açılan mesajlar için
+    // FirebaseMessaging.onMessageOpenedApp.listen((message) {
+    //   print('Bildirim tıklanarak açıldı');
+    //   // Burada kullanıcıyı ilgili sayfaya yönlendirebilirsin
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
-        RepositoryProvider<IGroupRepository>(
-          create: (_) => GroupService(),
-        ),
-        RepositoryProvider<IPhotoRepository>(
-          create: (_) => PhotoService(),
-        ),
+        RepositoryProvider<IGroupRepository>(create: (_) => GroupService()),
+        RepositoryProvider<IPhotoRepository>(create: (_) => PhotoService()),
       ],
       child: MultiBlocProvider(
         providers: [
           BlocProvider<AuthCubit>(
-            create: (context) => AuthCubit(secureStorage: secureStorage),
+            create: (context) => AuthCubit(secureStorage: widget.secureStorage),
           ),
           BlocProvider<GroupBloc>(
-            create: (context) =>
-                GroupBloc(context.read<IGroupRepository>()),
+            create: (context) => GroupBloc(context.read<IGroupRepository>()),
           ),
           BlocProvider<PhotoCubit>(
-            create: (context) =>
-                PhotoCubit(context.read<IPhotoRepository>()),
+            create: (context) => PhotoCubit(context.read<IPhotoRepository>()),
           ),
         ],
         child: MaterialApp(
